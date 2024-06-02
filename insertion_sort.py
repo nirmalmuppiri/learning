7b04e5fcfb54d7c85f9235e1daac164389d326c1
nirmalmuppiri <muppirinirmal@gmail.com>
2024-06-02 12:37:44 +0200

    new times

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,20 +1,18 @@
-#time: 01:14:93
+#time: 01:40:00
 
 class InsertionSort:
     def __init__(self, a):
         self.a = a
 
     def sort(self):
-        for p2 in range(1, len(self.a)):
-            item = self.a[p2]
-            p1 = p2 - 1
+        for i in range(1, len(self.a)):
+            item = self.a[i]
+            j = i -1
+            while j>=0 and item < self.a[j]:
+                self.a[j+1] = self.a[j]
+                j -=1
 
-            while p1 >= 0 and item < self.a[p1]:
-                self.a[p2] = self.a[p1]
-                p1 -=1
-
-            self.a[p1 + 1] = item
-
+            self.a[j+1] = item
 
 if __name__ == '__main__':
     array = [5,4,2,3]
